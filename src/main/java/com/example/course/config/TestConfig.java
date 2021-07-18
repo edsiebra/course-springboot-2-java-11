8be3214a6a3b00e25fc924ce8c9546a1fd955a39
Edson Siebra <edsiebra@gmail.com>
2021-07-18 15:47:13 -0300

    Subtotal total methods

--- src/main/java/com/example/course/config/TestConfig.java
+++ src/main/java/com/example/course/config/TestConfig.java
@@ -90,7 +90,7 @@ public class TestConfig implements CommandLineRunner{
 		
 		
 		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1); // para associação um para um não é chamado o repository do payment
-		o1.setPayment(pay1); // é necessarito setar na ordem do perdido 
+		o1.setPayment(pay1); // é necessarito setar na ordem do perdido
 		orderRepository.save(o1);
 		
 	}
